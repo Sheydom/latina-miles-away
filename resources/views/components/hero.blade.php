1ee9513d92c5fea30747187667f74c34a9f44085
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-03 21:11:20 +1100

    added. law cert for credibility

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,72 +1,76 @@
 <section class="bg-linear-to-br from-[primary-light] to-primary p-5 rounded-xl mb-10 mt-10">
-    <div class="flex justify-between">
-        <p class=" bg-blue-100 h-max p-1 text-xs rounded-2xl mb-5 w-fit">MELBOURNE BASED</p>
+    <div class="flex justify-end">
+        <p class=" bg-blue-100 h-max p-1 text-xs rounded-2xl mb-5 flex items-center"><svg class="w-5 h-5 shrink-0"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                <path fill="#cd7f4a"
+                    d="M192 284.4C256.1 269.9 304 212.5 304 144 304 64.5 239.5 0 160 0S16 64.5 16 144c0 68.5 47.9 125.9 112 140.4L128 480c0 17.7 14.3 32 32 32s32-14.3 32-32l0-195.6zM168 96c-30.9 0-56 25.1-56 56 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-57.4 46.6-104 104-104 13.3 0 24 10.7 24 24s-10.7 24-24 24z" />
+            </svg>MELBOURNE BASED</p>
     </div>
     <h1 class="mb-5">Your New <br>Horizon</h1>
     <p class="">Connecting the Latin community with Australian opportunities through expert migration pathways.</p>
-    <button type="button" data-cal-link="sheyla-solis-qbslzw/consultation"
-  data-cal-namespace="consultation"
-  data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":true}'
+    <button type="button" data-cal-link="sheyla-solis-qbslzw/consultation" data-cal-namespace="consultation"
+        data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":true}'
         class="w-full mt-6 rounded-2xl py-4 font-bold text-xl  text-amber-50 bg-[#4CC9D4] 
             shadow-[0_10px_25px_rgba(52,160,169,0.45)] border border-transparent  hover:from-[#2f8f97] hover:to-[#6fcfd8]
             hover:-translate-y-1 cursor-pointer">Start
         your journey -></button>
-                    <!-- Cal element-click embed code begins -->
-            <script type="text/javascript">
-                (function(C, A, L) {
-                    let p = function(a, ar) {
-                        a.q.push(ar);
+    <!-- Cal element-click embed code begins -->
+    <script type="text/javascript">
+        (function(C, A, L) {
+            let p = function(a, ar) {
+                a.q.push(ar);
+            };
+            let d = C.document;
+            C.Cal = C.Cal || function() {
+                let cal = C.Cal;
+                let ar = arguments;
+                if (!cal.loaded) {
+                    cal.ns = {};
+                    cal.q = cal.q || [];
+                    d.head.appendChild(d.createElement("script")).src = A;
+                    cal.loaded = true;
+                }
+                if (ar[0] === L) {
+                    const api = function() {
+                        p(api, arguments);
                     };
-                    let d = C.document;
-                    C.Cal = C.Cal || function() {
-                        let cal = C.Cal;
-                        let ar = arguments;
-                        if (!cal.loaded) {
-                            cal.ns = {};
-                            cal.q = cal.q || [];
-                            d.head.appendChild(d.createElement("script")).src = A;
-                            cal.loaded = true;
-                        }
-                        if (ar[0] === L) {
-                            const api = function() {
-                                p(api, arguments);
-                            };
-                            const namespace = ar[1];
-                            api.q = api.q || [];
-                            if (typeof namespace === "string") {
-                                cal.ns[namespace] = cal.ns[namespace] || api;
-                                p(cal.ns[namespace], ar);
-                                p(cal, ["initNamespace", namespace]);
-                            } else p(cal, ar);
-                            return;
-                        }
-                        p(cal, ar);
-                    };
-                })(window, "https://app.cal.com/embed/embed.js", "init");
-                Cal("init", "consultation", {
-                    origin: "https://app.cal.com"
-                });
+                    const namespace = ar[1];
+                    api.q = api.q || [];
+                    if (typeof namespace === "string") {
+                        cal.ns[namespace] = cal.ns[namespace] || api;
+                        p(cal.ns[namespace], ar);
+                        p(cal, ["initNamespace", namespace]);
+                    } else p(cal, ar);
+                    return;
+                }
+                p(cal, ar);
+            };
+        })(window, "https://app.cal.com/embed/embed.js", "init");
+        Cal("init", "consultation", {
+            origin: "https://app.cal.com"
+        });
 
 
-                // Important: Please add the following attributes to the element that should trigger the calendar to open upon clicking.
-                // `data-cal-link="sheyla-solis-qbslzw/consultation"`
-                // data-cal-namespace="consultation"
-                // `data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}'`
+        // Important: Please add the following attributes to the element that should trigger the calendar to open upon clicking.
+        // `data-cal-link="sheyla-solis-qbslzw/consultation"`
+        // data-cal-namespace="consultation"
+        // `data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":"true"}'`
 
 
 
-                Cal.ns.consultation("ui", {
-                    "cssVarsPerTheme": {
-                        "light": {
-                            "cal-brand": "#34a0a9"
-                        },
-                        "dark": {
-                            "cal-brand": "#34a0a9"
-                        }
-                    },
-                    "hideEventTypeDetails": false,
-                    "layout": "month_view"
-                });
-            </script>
-            <!-- Cal element-click embed code ends -->
+        Cal.ns.consultation("ui", {
+            "cssVarsPerTheme": {
+                "light": {
+                    "cal-brand": "#34a0a9"
+                },
+                "dark": {
+                    "cal-brand": "#34a0a9"
+                }
+            },
+            "hideEventTypeDetails": false,
+            "layout": "month_view"
+        });
+    </script>
+    <!-- Cal element-click embed code ends -->
 </section>

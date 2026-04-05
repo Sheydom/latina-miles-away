2182f40face713a2e9052301236ff608966dde5d
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-05 19:45:12 +1000

    layout for desktop: navbar done, hero needs rework limit width

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="flex bg-soft px-2.5 sticky top-0 z-50">
-    <div class="flex w-full justify-between">
-        <div class="flex gap-5  items-center">
+    <div class="flex w-full md:justify-between">
+        <div class="flex gap-5  items-center md:w-full ">
             <a class="cursor-pointer" href="{{ route('home') }}">
                 <img class="w-15 h-15 sm:w-15 sm:h-15 shrink-0  overflow-hidden"
                     src="{{ Vite::asset('resources/images/lma.svg') }}" alt="Latina Miles Away Logo">
@@ -10,40 +10,8 @@
                 <path
                     d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
             </svg> --}}
-            <button data-cal-link="sheyla-solis-qbslzw/consultation" data-cal-namespace="consultation"
-                data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":true}'
-                class="cursor-pointer p-1 rounded-xl
-          font-semibold text-white
-          bg-linear-to-r 
-          from-[#34A0A9] to-[#4CC9D4]
-          shadow-[0_6px_16px_rgba(52,160,169,0.35)]
-          hover:shadow-[0_8px_20px_rgba(52,160,169,0.5)]
-          hover:scale-[1.02]
-          transition duration-200">Enquire</button>
-            @if (request()->routeIs('legal.*') || request()->routeIs('contact'))
-                <a href="{{ route('home') }}"
-                    class="flex mr-5 cursor-pointer rounded-2xl p-1 text-primary items-center  hover:bg-primary-light group hover:text-white transition duration-500 ease-in-out">
-                    <svg class="h-5 w-5 shrink-0 text-primary group-hover:text-white transition duration-500 ease-in-out"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                        <path fill="currentColor"
-                            d="M341.8 72.6C329.5 61.2 310.5 61.2 298.3 72.6L74.3 280.6C64.7 289.6 61.5 303.5 66.3 315.7C71.1 327.9 82.8 336 96 336L112 336L112 512C112 547.3 140.7 576 176 576L464 576C499.3 576 528 547.3 528 512L528 336L544 336C557.2 336 569 327.9 573.8 315.7C578.6 303.5 575.4 289.5 565.8 280.6L341.8 72.6zM264 320C264 289.1 289.1 264 320 264C350.9 264 376 289.1 376 320C376 350.9 350.9 376 320 376C289.1 376 264 350.9 264 320zM208 496C208 451.8 243.8 416 288 416L352 416C396.2 416 432 451.8 432 496C432 504.8 424.8 512 416 512L224 512C215.2 512 208 504.8 208 496z" />
-                    </svg>
-                    <p>HOME</p>
-                </a>
-            @endif
-            {{-- Now</button><button
-                class="cursor-pointer p-1 rounded-xl border border-primary text-primary hover:text-white hover:bg-primary transition duration-200 ease-in-out">Ressources</button> --}}
-        </div>
+            <p class="hidden md:block">Latina Miles Away</p>
 
-        <div class="flex items-center gap-1">
-            {{-- <svg class="w-5 h-5 shrink-0 hidden" xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                <path
-                    d="M351.9 280l-190.9 0c2.9 64.5 17.2 123.9 37.5 167.4 11.4 24.5 23.7 41.8 35.1 52.4 11.2 10.5 18.9 12.2 22.9 12.2s11.7-1.7 22.9-12.2c11.4-10.6 23.7-28 35.1-52.4 20.3-43.5 34.6-102.9 37.5-167.4zM160.9 232l190.9 0C349 167.5 334.7 108.1 314.4 64.6 303 40.2 290.7 22.8 279.3 12.2 268.1 1.7 260.4 0 256.4 0s-11.7 1.7-22.9 12.2c-11.4 10.6-23.7 28-35.1 52.4-20.3 43.5-34.6 102.9-37.5 167.4zm-48 0C116.4 146.4 138.5 66.9 170.8 14.7 78.7 47.3 10.9 131.2 1.5 232l111.4 0zM1.5 280c9.4 100.8 77.2 184.7 169.3 217.3-32.3-52.2-54.4-131.7-57.9-217.3L1.5 280zm398.4 0c-3.5 85.6-25.6 165.1-57.9 217.3 92.1-32.7 159.9-116.5 169.3-217.3l-111.4 0zm111.4-48C501.9 131.2 434.1 47.3 342 14.7 374.3 66.9 396.4 146.4 399.9 232l111.4 0z" />
-            </svg> --}}
-
-            <button type="button" class="cursor-pointer">ES|EN</button>
             <!-- Cal element-click embed code begins -->
             <script type="text/javascript">
                 (function(C, A, L) {
@@ -102,6 +70,43 @@
                 });
             </script>
             <!-- Cal element-click embed code ends -->
+            @if (request()->routeIs('legal.*') || request()->routeIs('contact'))
+                <a href="{{ route('home') }}"
+                    class="flex mr-5 cursor-pointer rounded-2xl p-1 text-primary items-center  hover:bg-primary-light group hover:text-white transition duration-500 ease-in-out">
+                    <svg class="h-5 w-5 shrink-0 text-primary group-hover:text-white transition duration-500 ease-in-out"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                        <path fill="currentColor"
+                            d="M341.8 72.6C329.5 61.2 310.5 61.2 298.3 72.6L74.3 280.6C64.7 289.6 61.5 303.5 66.3 315.7C71.1 327.9 82.8 336 96 336L112 336L112 512C112 547.3 140.7 576 176 576L464 576C499.3 576 528 547.3 528 512L528 336L544 336C557.2 336 569 327.9 573.8 315.7C578.6 303.5 575.4 289.5 565.8 280.6L341.8 72.6zM264 320C264 289.1 289.1 264 320 264C350.9 264 376 289.1 376 320C376 350.9 350.9 376 320 376C289.1 376 264 350.9 264 320zM208 496C208 451.8 243.8 416 288 416L352 416C396.2 416 432 451.8 432 496C432 504.8 424.8 512 416 512L224 512C215.2 512 208 504.8 208 496z" />
+                    </svg>
+                    <p>HOME</p>
+                </a>
+            @endif
+            {{-- Now</button><button
+                class="cursor-pointer p-1 rounded-xl border border-primary text-primary hover:text-white hover:bg-primary transition duration-200 ease-in-out">Ressources</button> --}}
+        </div>
+
+        <div class="flex items-center gap-1">
+            <button class="text-primary hover:bg-primary rounded-2xl p-1 cursor-pointer transition duration-200 ease-in-out hover:text-white hidden md:block">Ressources</button>
+            <button data-cal-link="sheyla-solis-qbslzw/consultation"
+                data-cal-namespace="consultation"
+                data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":true}'
+                class="cursor-pointer p-1 rounded-xl 
+          font-semibold text-white
+          bg-linear-to-r 
+          from-[#34A0A9] to-[#4CC9D4]
+          shadow-[0_6px_16px_rgba(52,160,169,0.35)]
+          hover:shadow-[0_8px_20px_rgba(52,160,169,0.5)]
+          hover:scale-[1.02]
+          transition duration-200">Enquire</button>
+            {{-- <svg class="w-5 h-5 shrink-0 hidden" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
+                <path
+                    d="M351.9 280l-190.9 0c2.9 64.5 17.2 123.9 37.5 167.4 11.4 24.5 23.7 41.8 35.1 52.4 11.2 10.5 18.9 12.2 22.9 12.2s11.7-1.7 22.9-12.2c11.4-10.6 23.7-28 35.1-52.4 20.3-43.5 34.6-102.9 37.5-167.4zM160.9 232l190.9 0C349 167.5 334.7 108.1 314.4 64.6 303 40.2 290.7 22.8 279.3 12.2 268.1 1.7 260.4 0 256.4 0s-11.7 1.7-22.9 12.2c-11.4 10.6-23.7 28-35.1 52.4-20.3 43.5-34.6 102.9-37.5 167.4zm-48 0C116.4 146.4 138.5 66.9 170.8 14.7 78.7 47.3 10.9 131.2 1.5 232l111.4 0zM1.5 280c9.4 100.8 77.2 184.7 169.3 217.3-32.3-52.2-54.4-131.7-57.9-217.3L1.5 280zm398.4 0c-3.5 85.6-25.6 165.1-57.9 217.3 92.1-32.7 159.9-116.5 169.3-217.3l-111.4 0zm111.4-48C501.9 131.2 434.1 47.3 342 14.7 374.3 66.9 396.4 146.4 399.9 232l111.4 0z" />
+            </svg> --}}
+
+            {{-- <button type="button" class="cursor-pointer">ES|EN</button> --}}
+
         </div>
     </div>
 </nav>
--- resources/views/components/services.blade.php
+++ resources/views/components/services.blade.php
@@ -1,141 +1,119 @@
-<section id="services" class="w-full mb-10 items-center">
+<section id="services" class="w-full mb-10 items-center ">
     <div class="flex flex-col items-center p-2">
         <h2>Our Services</h2>
         <p class="mb-5">We provide guidance and support throughout your journey in Australia.</p>
     </div>
 
 
-    <div id="serviceContainer" style="-webkit-overflow-scrolling: touch;"
-        class="flex  gap-4 overflow-x-scroll services pb-2 scrollbar-visible snap-x snap-mandatory">
-        <div
-            class="w-[85vw] max-w-xs min-h-60  shrink-0 rounded-2xl snap-center 
-border border-primary
-bg-white 
-p-5 
-text-center 
-shadow-md
-transition">
-
-            <div class="text-2xl mb-3">🧭</div>
-
-            <h3 class="font-semibold text-sm mb-2 ">
-                Migration Support Consultation
-            </h3>
-
-            <p class="text-xs text-neutral-600">
-                One-on-one session to review your situation
-                and explain available public information.
-            </p>
-
-        </div>
-        <div
-            class="w-[85vw] max-w-xs min-h-60 shrink-0 rounded-2xl snap-center border borde border-neutral-200  bg-white  p-5 
-                text-center 
-                shadow-sm 
-                hover:shadow-md 
-                transition">
-
-            <div class="text-2xl mb-3">
-                📋
+    <div class="flex w-full justify-center">
+        <div id="serviceContainer" style="-webkit-overflow-scrolling: touch;"
+            class="flex  gap-4 overflow-x-scroll services pb-2 scrollbar-visible snap-x snap-mandatory max-w-6xl   md:overflow-x-hidden md:grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))]">
+            <div
+                class="w-[85vw] max-w-xs min-h-60 shrink-0 rounded-2xl snap-center
+                    border border-primary
+                    bg-white
+                    p-5
+                    text-center
+                    shadow-md
+                    transition md:w-full md:max-w-none md:shrink">
+                <div class="text-2xl mb-3">🧭</div>
+                <h3 class="font-semibold text-sm mb-2 ">
+                    Migration Support Consultation
+                </h3>
+                <p class="text-xs text-neutral-600">
+                    One-on-one session to review your situation
+                    and explain available public information.
+                </p>
             </div>
-
-            <h3 class="font-semibold text-sm mb-2">
-                Document Review
-            </h3>
-
-            <p class="text-xs text-neutral-600 leading-relaxed">
-                Review your prepared documents for clarity,
-                formatting, and completeness.
-            </p>
-
-        </div>
-        <div
-            class="w-[85vw] max-w-xs min-h-60  shrink-0  rounded-2xl snap-center 
-border border-neutral-200 
-bg-white 
-p-5 
-text-center 
-shadow-sm 
-hover:shadow-md 
-transition">
-
-            <div class="text-2xl mb-3">🧾</div>
-
-            <h3 class="font-semibold text-sm mb-2">
-                TFN & ABN Guidance
-            </h3>
-
-            <p class="text-xs text-neutral-600">
-                Step-by-step help understanding
-                application requirements.
-            </p>
-
-        </div>
-        <div
-            class="w-[85vw] max-w-xs min-h-60  shrink-0 rounded-2xl snap-center 
-border border-neutral-200 
-bg-white 
-p-5 
-text-center 
-shadow-sm 
-hover:shadow-md 
-transition">
-
-            <div class="text-2xl mb-3">📑</div>
-
-            <h3 class="font-semibold text-sm mb-2">
-                Resume Review
-            </h3>
-
-            <p class="text-xs text-neutral-600">
-                Improve your resume to match
-                Australian expectations.
-            </p>
-
-        </div>
-        <div
-            class="w-[85vw] max-w-xs min-h-60  shrink-0 rounded-2xl snap-center 
-border border-neutral-200 
-bg-white 
-p-5 
-text-center 
-shadow-sm 
-hover:shadow-md 
-transition">
-
-            <div class="text-2xl mb-3">💬</div>
-
-            <h3 class="font-semibold text-sm mb-2">
-                Migration Information Session
-            </h3>
-
-            <p class="text-xs text-neutral-600">
-                General discussion using
-                publicly available information.
-            </p>
-
-        </div>
-        <div
-            class="w-[85vw] max-w-xs min-h-60  shrink-0  rounded-2xl snap-center 
-border border-neutral-200 
-bg-white 
-p-5 
-text-center 
-shadow-sm 
-hover:shadow-md 
-transition">
-
-            <div class="text-2xl mb-3">🎯</div>
-
-            <h3 class="font-semibold text-sm mb-2">
-                Work Readiness Package
-            </h3>
-
-            <p class="text-xs text-neutral-600">
-                TFN guidance, resume review,
-                and workplace preparation.
-            </p>
-
+            <div
+                class="w-[85vw] max-w-xs min-h-60 shrink-0 rounded-2xl snap-center border borde border-neutral-200  bg-white  p-5
+                    text-center
+                    shadow-sm
+                    hover:shadow-md
+                    transition md:w-full md:max-w-none md:shrink">
+                <div class="text-2xl mb-3">
+                    📋
+                </div>
+                <h3 class="font-semibold text-sm mb-2">
+                    Document Review
+                </h3>
+                <p class="text-xs text-neutral-600 leading-relaxed">
+                    Review your prepared documents for clarity,
+                    formatting, and completeness.
+                </p>
+            </div>
+            <div
+                class="w-[85vw] max-w-xs min-h-60  shrink-0  rounded-2xl snap-center
+                    border border-neutral-200
+                    bg-white
+                    p-5
+                    text-center
+                    shadow-sm
+                    hover:shadow-md
+                    transition md:w-full md:max-w-none md:shrink">
+                <div class="text-2xl mb-3">🧾</div>
+                <h3 class="font-semibold text-sm mb-2">
+                    TFN & ABN Guidance
+                </h3>
+                <p class="text-xs text-neutral-600">
+                    Step-by-step help understanding
+                    application requirements.
+                </p>
+            </div>
+            <div
+                class="w-[85vw] max-w-xs min-h-60  shrink-0 rounded-2xl snap-center
+                    border border-neutral-200
+                    bg-white
+                    p-5
+                    text-center
+                    shadow-sm
+                    hover:shadow-md
+                    transition md:w-full md:max-w-none md:shrink">
+                <div class="text-2xl mb-3">📑</div>
+                <h3 class="font-semibold text-sm mb-2">
+                    Resume Review
+                </h3>
+                <p class="text-xs text-neutral-600">
+                    Improve your resume to match
+                    Australian expectations.
+                </p>
+            </div>
+            <div
+                class="w-[85vw] max-w-xs min-h-60  shrink-0 rounded-2xl snap-center
+                        border border-neutral-200
+                        bg-white
+                        p-5
+                        text-center
+                        shadow-sm
+                        hover:shadow-md
+                        transition md:w-full md:max-w-none md:shrink">
+                <div class="text-2xl mb-3">💬</div>
+                <h3 class="font-semibold text-sm mb-2">
+                    Migration Information Session
+                </h3>
+                <p class="text-xs text-neutral-600">
+                    General discussion using
+                    publicly available information.
+                </p>
+            </div>
+            <div
+                class="w-[85vw] max-w-xs min-h-60  shrink-0  rounded-2xl snap-center
+                    border border-neutral-200
+                    bg-white
+                    p-5
+                    text-center
+                    shadow-sm
+                    hover:shadow-md
+                    transition md:w-full md:max-w-none md:shrink">
+                <div class="text-2xl mb-3">🎯</div>
+                <h3 class="font-semibold text-sm mb-2">
+                    Work Readiness Package
+                </h3>
+                <p class="text-xs text-neutral-600">
+                    TFN guidance, resume review,
+                    and workplace preparation.
+                </p>
+            </div>
         </div>
     </div>
 

aeb8ba36a33326173080796dfc5f9ccd38b33d87
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-06 20:48:39 +1000

    layout: for service 1:1 session completed

--- resources/views/components/secure.blade.php
+++ resources/views/components/secure.blade.php
@@ -39,13 +39,13 @@
                     <svg class="w-5 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                         <path fill="#cd7f4a" d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z"/>
                     </svg>
-                    <p><strong>Peace of Mind:</strong> Your life story and identity are safe from third-party access.</p>
+                    <p><strong>Peace of Mind:</strong><br class="hidden md:block"> Your life story and identity are safe from third-party access.</p>
                 </li>
                 <li class="flex gap-3">
                     <svg class="w-5 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                         <path fill="#cd7f4a" d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z"/>
                     </svg>
-                    <p><strong>Privacy First:</strong> All information shared during consultations is treated respectfully and confidentially.</p>
+                    <p><strong>Privacy First:</strong> <br class="hidden md:block"> All shared information is treated respectfully and confidentially.</p>
                 </li>
             </ul>
         </div>
--- resources/views/services/guidanceSession.blade.php
+++ resources/views/services/guidanceSession.blade.php
@@ -1,57 +1,81 @@
 <x-layout.app>
     <section class="w-full text-center flex flex-col items-center">
-        <div class="bg-white rounded-2xl shadow-md border border-neutral-200 p-5 mb-6">
+        <div class="w-full bg-primary p-2.5 md:p-15 md:grid md:grid-cols-2">
+            <div
+                class="w-full mx-auto  bg-white rounded-tl-2xl rounded-tr-2xl md:text-left md:rounded-tr-none md:rounded-bl-2xl shadow-md p-6 md:p-25">
+                <!-- Title -->
+                <h2 class="text-xl md:text-3xl font-semibold text-center md:text-left mb-2">
+                    1:1 Guidance Session
+                </h2>
+                <p class="text-sm md:text-base text-neutral-500 text-center md:text-left mb-6">
+                    Clarity, direction, and real-world guidance for your migration journey.
+                </p>
+                <!-- Who it's for -->
+                <div class="mb-5">
+                    <h3 class="font-semibold text-sm md:text-2xl mb-2 text-primary">
+                        Who it's for
+                    </h3>
+                    <p class="text-sm md:text-base text-neutral-700">
+                        If you're planning to move to Australia or already here and feel unsure about your next steps.
+                    </p>
+                </div>
+                <!-- What we talk about -->
+                <div class="mb-5">
+                    <h3 class="font-semibold text-sm md:text-2xl mb-2 text-primary">
+                        What we can talk about
+                    </h3>
+                    <ul class="space-y-2 text-sm md:text-base text-neutral-700">
+                        <li>• Your visa pathway (based on real experience)</li>
+                        <li>• What documents you may need</li>
+                        <li>• How to prepare your application process</li>
+                        <li>• Job and settlement strategies</li>
+                        <li>• How to avoid common mistakes</li>
+                    </ul>
+                </div>
 
-            <h2 class=" font-semibold mb-4">
-                What to expect during your consultation
-            </h2>
-
-            <ul class="space-y-2 text-neutral-700 text-sm flex flex-col">
-
-                <li class="flex gap-3 text-left "> <svg class="w-5 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                        <path fill="#cd7f4a"
-                            d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z" />
-                    </svg>
-                    <p>Review your current situation and available information</p>
-                </li>
-
-                <li class="flex gap-3 text-left "><svg class="w-5 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                        <path fill="#cd7f4a"
-                            d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z" />
-                    </svg>
-                    <p>Discuss general visa pathways using publicly available information</p>
-                </li>
-
-                <li class="flex gap-3 text-left"><svg class="w-5 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                        <path fill="#cd7f4a"
-                            d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z" />
-                    </svg> Explain general requirements, costs, and expected timelines</li>
-
-                <li class="flex gap-3 text-left"><svg class="w-5 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                        <path fill="#cd7f4a"
-                            d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z" />
-                    </svg>Answer your questions clearly</li>
-
-                <li class="flex gap-3 text-left"><svg class="w-5 h-5 shrink-0" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 640 640"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
-                        <path fill="#cd7f4a"
-                            d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z" />
-                    </svg> Provide general next steps to help you understand your options</li>
-
-            </ul>
-
-            <p class="mt-4 text-sm text-neutral-600">
-                This consultation is ideal if you are looking for detailed guidance and personalised advice.
-            </p>
+                <!-- What you get -->
+                <div class="mb-6">
+                    <h3 class="font-semibold text-sm md:text-2xl mb-2 text-primary">
+                        What you get
+                    </h3>
+                    <ul class="space-y-2 text-sm md:text-base text-neutral-700">
+                        <li>• Personalised guidance</li>
+                        <li>• Clear next steps</li>
+                        <li>• Honest insights based on real situations</li>
+                    </ul>
+                </div>
+                <!-- Final trust line -->
+                <p class="text-sm md:text-base text-neutral-600 italic text-center">
+                    “This session is designed to give you clarity and direction — so you don’t waste time, money, or
+                    make
+                    unnecessary mistakes.”
+                </p>
+            </div>
+            <div class="w-full bg-white md:rounded-r-2xl md:flex md:flex-col md:items-center md:p-25 ">
+                <div class=""><img class="md:rounded-2xl md:max-h-100"
+                        src="{{ Vite::asset('resources/images/profile.jpg') }}" alt="profilePhoto">
+                </div>
+                <div class="bg-white rounded-br-2xl rounded-bl-2xl md:rounded-bl-none">
+                    <!-- Experience -->
+                    <div class="mb-5 md:text-left">
+                        <h3 class="font-semibold text-sm md:text-2xl mb-2 text-primary">
+                            My experience
+                        </h3>
+                        <ul class="space-y-2 text-sm md:text-base text-neutral-700">
+                            <li>• Student visa</li>
+                            <li>• COVID visa (408)</li>
+                            <li>• Partner sponsorship</li>
+                            <li>• Permanent Residency</li>
+                        </ul>
+                        <p class="text-sm md:text-base text-neutral-600 mt-2">
+                            I understand the process from real experience — not theory.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
 
-
-
-        <div class="text-center mt- mb-6">
+        <div class="text-center mt-6 mb-6">
 
             <p class="text-sm text-neutral-600 mb-2">
 

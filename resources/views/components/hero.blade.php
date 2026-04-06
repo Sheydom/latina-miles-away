9267691c3087c8fb37f3e78638411949495abb0f
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-06 14:33:25 +1000

    layout cards added all icons

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -7,11 +7,10 @@
                     viewBox="0 0 320 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.-->
                     <path fill="#cd7f4a"
                         d="M192 284.4C256.1 269.9 304 212.5 304 144 304 64.5 239.5 0 160 0S16 64.5 16 144c0 68.5 47.9 125.9 112 140.4L128 480c0 17.7 14.3 32 32 32s32-14.3 32-32l0-195.6zM168 96c-30.9 0-56 25.1-56 56 0 13.3-10.7 24-24 24s-24-10.7-24-24c0-57.4 46.6-104 104-104 13.3 0 24 10.7 24 24s-10.7 24-24 24z" />
-                </svg>Melbourne-based</p>
+                </svg>Based in Melbourne</p>
         </div>
-        <h1 class="mb-5 md:text-7xl">Your New <br>Horizon</h1>
-        <p class="md:max-w-100">Connecting the Latin community with Australian opportunities through guidance and
-            reliable support.
+        <h1 class="mb-5 md:text-7xl">Everything <br class="sm:hidden"> you need <br class="hidden sm:block"> to <br class="sm:hidden">start in Australia</h1>
+        <p class="md:max-w-100">Real guidance from someone who has been in your shoes — helping you avoid mistakes and make smarter decisions from day one.
         </p>
         <button type="button" data-cal-link="sheyla-solis-qbslzw/consultation" data-cal-namespace="consultation"
             data-cal-config='{"layout":"month_view","useSlotsViewOnSmallScreen":true}'

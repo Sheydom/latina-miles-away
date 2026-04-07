972392f87ed9e78736b7a638be6f46d9232bd1b5
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-07 19:48:45 +1000

    style: 1on1-y-space bullet points

--- resources/views/services/guidanceSession.blade.php
+++ resources/views/services/guidanceSession.blade.php
@@ -1,6 +1,6 @@
 <x-layout.app>
     <section class="w-full text-center bg-linear-to-b from-primary to-#EAEFEF flex flex-col items-center">
-        <div class="w-full md:w-max  p-5 md:px-5 md:grid md:grid-cols-[400px_max(500px)] ">
+        <div class="w-full md:w-max  p-5 md:px-5 md:grid md:grid-cols-[400px_max(600px)] ">
             <div
                 class="w-fit mx-auto  bg-white rounded-tl-2xl rounded-tr-2xl text-left md:text-left md:rounded-tr-none md:rounded-bl-2xl shadow-md p-5">
                 <!-- Title -->
@@ -12,7 +12,7 @@
                 </p>
                 <!-- Who it's for -->
                 <div class="mb-5">
-                    <h3 class="font-semibold text-lg md:text-2xl mb-2 text-primary">
+                    <h3 class="font-semibold text-lg md:text-2xl mb-2">
                         Who it's for
                     </h3>
                     <p class="text-sm md:text-base text-neutral-700">
@@ -21,7 +21,7 @@
                 </div>
                 <!-- What we talk about -->
                 <div class="mb-5">
-                    <h3 class="font-semibold text-lg md:text-2xl mb-2 text-primary">
+                    <h3 class="font-semibold text-lg md:text-2xl mb-2 ">
                         What we can talk about
                     </h3>
                     <ul class="space-y-2 text-sm md:text-base text-neutral-700 list-disc marker:text-tertiary px-5">
@@ -35,7 +35,7 @@
 
                 <!-- What you get -->
                 <div class="mb-6">
-                    <h3 class="font-semibold text-lg md:text-2xl mb-2 text-primary ">
+                    <h3 class="font-semibold text-lg md:text-2xl mb-2 ">
                         What you get
                     </h3>
                     <ul class="space-y-2 text-sm md:text-base text-neutral-700 list-disc marker:text-tertiary px-5">
@@ -62,8 +62,8 @@
                             <h3 class="font-semibold text-lg md:text-2xl mb-2 ">
                                 My experience
                             </h3>
-                            <ul class="space-y-2 md:space-y-0 flex md:flex-col justify-around  text-left text-sm md:text-base text-neutral-700 ">
-                                <div>
+                            <ul class="space-y-2  flex md:flex-col justify-around  text-left text-sm md:text-base text-neutral-700 ">
+                                <div class="md:space-y-2">
                                     <li class="flex items-center"><svg class="w-5 h-5 shrink-0 text-primary " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                             <path fill="currentColor" d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z"/>
                         </svg>Partner sponsorship</li>
@@ -71,7 +71,7 @@
                             <path fill="currentColor" d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z"/>
                         </svg>Permanent Residency</li>
                                 </div>
-                                <div>
+                                <div class="md:space-y-2">
                                     <li class="flex items-center"> <svg class="w-5 h-5 shrink-0 text-primary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                             <path fill="currentColor" d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z"/>
                         </svg>COVID visa (408)</li>

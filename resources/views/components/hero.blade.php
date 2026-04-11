ecd46bd8289c6c6f022eb614ed3bc8d0cbf80c35
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-11 16:28:26 +1000

    typo

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -24,5 +24,5 @@
 
     </div>
     <img class="w-120 rounded-2xl hidden md:flex md:w-auto md:h-80 rotate-3"
-        src="{{ asset('images/melbourne.webp) }}" alt="melbourne">
+        src="{{ asset('images/melbourne.webp') }}" alt="melbourne">
 </section>

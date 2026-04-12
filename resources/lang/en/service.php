21e651fc20afa96aa6717956703ca64437c15db6
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-12 16:08:38 +1000

    translation: added service tiles en file

---
+++ resources/lang/en/service.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'title' => 'How I Can Support Your Journey',
+    'subtTitle' => 'Whether you`re just getting started or need specific support, here are the ways I can help you move forward with confidence.',
+    'mostPopular' => 'Most Popular',
+    'guidanceTitle' => '1:1 Guidance Session',
+    'guidanceParagraph' => 'Get personalised support, clear answers, and practical advice tailored to your situation — so you can move forward with confidence.',
+    'guidanceLearnMore' => 'Learn More ->',
+    'accomodationTitle' => 'Accomodation Guide - Free',
+    'accomodationParagraph' => 'Learn how to find your first place in Australia, avoid common scams, and make safe housing decisions.',
+    'accomodationLearnMore' => 'Learn More',
+    'resumeReviewTitle' => 'Resume Review',
+    'resumeReviewParagraph' => 'Get personalised feedback to ensure your resume matches Australian employer expectations and improves your chances of getting interviews.',
+    'resumeReviewLearnMore' => 'Learn More ->',
+    'documentReviewTitle' => 'Document Review',
+    'documentParagraph' => 'Get clear, practical feedback to improve your documents in English — including resumes, cover letters, and application forms.',
+    'documentLearnMore' => 'Learn More ->',
+    'mostValue' => 'Most Value',
+    'workReadiness' => 'Work Readiness Package',
+    'workReadinessParagraph' => 'TFN setup guidance, resume review, and bank account support to help you get job-ready faster.',
+    'workReadinessLearnMore' => 'Learn More ->',
+    'swipe' => 'Swipe to see more <span class="text-white flex w-5 h-5 items-center bg-tertiary rounded-full p-1">→</span>',
+];

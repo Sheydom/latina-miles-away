ecb253a29c7b70f83a5774f19b57add2faebfe14
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-12 12:34:21 +1000

    test3

--- resources/views/components/trust.blade.php
+++ resources/views/components/trust.blade.php
@@ -22,7 +22,7 @@
                         class="inline-flex items-center rounded-full border border-transparent bg-white/80 px-2 py-0.5 underline transition cursor-pointer hover:bg-primary hover:text-white md:hover:border-primary-light">
                         qualified
                     </button>
-                    as a <strong>Lawyer from Peruu</strong>, I arrived in Australia with the same dreams and
+                    as a <strong>Lawyer from Peru</strong>, I arrived in Australia with the same dreams and
                     uncertainties you might be feeling today. My path to residency was not a straight line — it was a
                     journey of resilience.
                 </p>

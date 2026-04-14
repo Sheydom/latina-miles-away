503e82c4192599c30e4a892d47cc0e463b6364dc
Dominic Knabe <dominic.knabe@gmail.com>
2026-04-14 22:12:15 +1000

    typo

--- resources/lang/es/startHere.php
+++ resources/lang/es/startHere.php
@@ -14,7 +14,7 @@ return [
 
         'arrivalHeader' => 'Checklist: Tus primeros 7 días en Australia',
 
-        'arrivalHeaderPragraph' => 'Una lista práctica para ayudarte a organizarte durante tu primera semana. Incluye los pasos más importantes para que puedas establecerte con más confianza y menos estrés.',
+        'arrivalHeaderParagraph' => 'Una lista práctica para ayudarte a organizarte durante tu primera semana. Incluye los pasos más importantes para que puedas establecerte con más confianza y menos estrés.',
 
         'previewButton' => 'Ver guía',
 
@@ -26,7 +26,7 @@ return [
 
         'workHeader' => 'Guía para Encontrar Trabajo',
 
-        'workHeaderPragraph' => 'Una guía sencilla en español para quienes desean entender lo básico sobre cómo encontrar trabajo en Australia. Clara, práctica y fácil de seguir.',
+        'workHeaderParagraph' => 'Una guía sencilla en español para quienes desean entender lo básico sobre cómo encontrar trabajo en Australia. Clara, práctica y fácil de seguir.',
 
         'previewButton' => 'Ver guía',
 

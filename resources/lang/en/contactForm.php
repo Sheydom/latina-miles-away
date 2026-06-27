a3fece3b5a2faf21354affd9560d9a12d431bb47
Dominic Knabe <dominic.knabe@gmail.com>
2026-06-28 00:09:57 +1000

    lang files, changes spoken perspective

--- resources/lang/en/contactForm.php
+++ resources/lang/en/contactForm.php
@@ -2,7 +2,7 @@
 
 return [
     'title' => 'If you need more information',
-    'titleP' => 'Send us your details and we will get back to you as soon as possible.',
+    'titleP' => 'Send me your details and I will get back to you as soon as possible.',
     'name' => 'Name',
     'email' => 'Email',
     'phone' => 'Phone Number',
--- resources/lang/es/contactForm.php
+++ resources/lang/es/contactForm.php
@@ -2,7 +2,7 @@
 
 return [
     'title' => 'Si necesitas más información',
-    'titleP' => 'Envíanos tus datos y nos pondremos en contacto contigo lo antes posible.',
+    'titleP' => 'Envíame tus datos y me pondré en contacto contigo lo antes posible.',
     'name' => 'Nombre',
     'email' => 'Correo electrónico',
     'phone' => 'Número de teléfono',

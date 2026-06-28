cc05eb85c8a06c402203ebd6859258bc26460916
Dominic Knabe <dominic.knabe@gmail.com>
2026-06-28 18:52:47 +1000

    lang change carousel

--- resources/views/components/serviceCards.blade.php
+++ resources/views/components/serviceCards.blade.php
@@ -125,8 +125,8 @@
 </section>
 <section id="" class=" sm:hidden w-full mt-20 items-center ">
     <div class="flex flex-col items-center p-2">
-        <h2>{{ __('service.title') }}</h2>
-        <p class="mb-5">{{ __('service.subtitle') }}</p>
+        <h2>{{ __('service.titleC') }}</h2>
+        <p class="mb-5">{{ __('service.subtitleC') }}</p>
     </div>
 
 
